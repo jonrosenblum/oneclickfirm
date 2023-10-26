d44f6284c75d3a4022ad746cda6c01ff670d63b2
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-26 18:43:07 -0400

    Login and Signup Routes Set Up

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Root from './Routes/Root';
+import Login from './Routes/Login';
+import SignUp from './Routes/SignUp';
+
+const router = createBrowserRouter([
+  {path: '/', 
+  element: <Root />, 
+  children: [
+    {path: '/login', element: <Login />},
+    {path: '/sign-up', element: <SignUp />},
+  ]}
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
