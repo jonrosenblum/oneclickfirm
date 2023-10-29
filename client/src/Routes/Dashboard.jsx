7d02c886379cd3febc021f8ee378485cd749f7e8
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-29 14:57:48 -0400

    Updates

--- client/src/Routes/Dashboard.jsx
+++ client/src/Routes/Dashboard.jsx
@@ -3,47 +3,47 @@ import "../Styles/dashboard.css"
 
 export default function Dashboard() {
     return(
-        <div className="centered-content">
-            <div className="task-manager-container">
+        <div className="dashboard-content">
+            <div className="main-container">
                 <div className="sidebar has-background-white">
                     <h1 className="title">
                         Clients
                     </h1>
 
-                    <div className="list-menu">
-                        <a className="list-menu-item is-active">
+                    <div className="client-menu">
+                        <a className="client-menu-item is-active">
                             <p>Steven Stabile</p>
                         </a>
-                        <a className="list-menu-item">
+                        <a className="client-menu-item">
                             <p>Colene Stabile</p>
                         </a>
-                        <a className="list-menu-item">
-                            <p>Stephanie Rosenblum</p>
+                        <a className="client-menu-item">
+                            <p>Jon Rosenblum</p>
                         </a>
-                        <a className="list-menu-item">
+                        <a className="client-menu-item">
                             <p>Barbara Stabile</p>
                         </a>
                     </div>
 
-                    <button className="button">+ New Client</button>
+                    <button className="client-button button">+ New Client</button>
 
                 </div>
 
-                <div className="task-list-container has-background-light">
+                <div className="client-information-container has-background-light">
                     <h1 className="title">
                         Client Information
                     </h1>
 
-                    <div className="task">
+                    <div className="document">
                         <p>Credit Card Authorization Form</p>
                     </div>
-                    <div className="task">
+                    <div className="document">
                         <p>Representation Letter Form</p>
                     </div>
-                    <div className="task">
+                    <div className="document">
                         <p>Discovery Letter Form</p>
                     </div>
-                    <div className="task">
+                    <div className="document">
                         <p>Retainer Agreement Form</p>
                     </div>
                 </div>
