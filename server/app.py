4b20c4ef8e2abd7dc66a0ea066f667015a52d573
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-28 21:29:57 -0400

    PGSQL Connection Fixed

--- server/app.py
+++ server/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes import auth_bp, client_scraper_bp, document_templates_bp
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_username:your_password@localhost/document_templates'
+db = SQLAlchemy(app)
+
 
 app.register_blueprint(client_scraper_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(document_templates_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
