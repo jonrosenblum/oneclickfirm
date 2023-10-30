b6bdcfe30ba9a642532baa93e47143f59d7b7c1d
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-30 16:03:06 -0400

    Menu Updates

--- client/src/Components/SideNav.jsx
+++ client/src/Components/SideNav.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { BsArrowLeftShort, BsSearch, BsChevronDown, BsFillImageFill, BsReverseLayoutTextSidebarReverse, BsPerson} from "react-icons/bs";
-import { AiFillEnvironment, AiOutlineBarChart, AiOutlineFileText, AiOutlineLogout, AiOutlineMail, AiOutlineSetting } from "react-icons/ai";
+import { BsArrowLeftShort, BsSearch, BsChevronDown, BsFillImageFill,BsFillPeopleFill, BsPerson} from "react-icons/bs";
+import { AiOutlineBarChart, AiOutlineFileText, AiOutlineLogout, AiOutlineMail, AiOutlineSetting } from "react-icons/ai";
 import { RiDashboardFill } from "react-icons/ri";
+import {GiInjustice} from "react-icons/gi";
 
 export default function SideNav() {
 
@@ -9,13 +10,11 @@ export default function SideNav() {
     const [submenuOpen, setSubmenuOpen] = useState(false)
     const Menus = [
         {title: "Dashboard"},
-        {title: "Pages", icon: <AiOutlineFileText/>},
+        {title: "Documents", icon: <AiOutlineFileText/>},
         {title: "Media", spacing: true, icon: <BsFillImageFill/>},
-        {title: "Projects", submenu: true, icon: <BsReverseLayoutTextSidebarReverse/>,
+        {title: "Clients", submenu: true, icon: <BsFillPeopleFill/>,
         submenuItems: [
-            {title: "Project 1"}, 
-            {title: "Project 2"}, 
-            {title: "Project 3"}]},
+            {title: "+ New Client"}]},
         {title: "Analytics", icon: <AiOutlineBarChart/>},
         {title: "Inbox", icon: <AiOutlineMail/>},
         {title: "Profile", spacing: true, icon: <BsPerson/>},
@@ -28,27 +27,27 @@ export default function SideNav() {
         <div className="flex">
             <div className={`font-oswald bg-gradient-to-tr from-blue-800 to-green-400 h-screen p-5 pt-8 
             ${open ? "w-72": "w-20"} duration-300 relative`}>
-            <BsArrowLeftShort className={`bg-white text-dark-purple text-3xl rounded-full 
+            <BsArrowLeftShort className={`bg-white text-3xl rounded-full 
             absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} onClick={() => {
                 setOpen(!open)
             }}/>
             <div className="inline-flex">
-                <AiFillEnvironment className={`bg-amber-300 text-4xl
+                <GiInjustice className={`bg-amber-300 text-4xl
                 rounded cursor-pointer block float-left mr-2 duration-500 ${open && "rotate-[360deg]"}`}/>
                 <h1 className={`text-white origin-left font-medium text-2xl duration-300 
-                ${!open && "scale-0"}`}>Tailwind</h1>
+                ${!open && "scale-0"}`}>Stabile Law Firm</h1>
             </div>
             <div className={`flex items-center rounded-md
             bg-light-white mt-6 ${!open ? "px-4" : "px-2.5"} py-2`}>
                 <BsSearch className={`text-white text-lg block float-left cursor-pointer ${open && "mr-2"}`}/>
-                <input type="{search}" placeholder="Search" className={`text-base bg-transparent 
+                <input type="{search}" placeholder="" className={`text-base bg-transparent 
                 w-full text-white focus:outline-none ${!open && "hidden" }`} />
             </div>
 
             <ul className="pt-2">
                 {Menus.map((menu, index) => (
                 <>
-                    <li key={index} className={`text-gray-300 text-sm flex items-center 
+                    <li key={index} className={`text-white text-sm flex items-center 
                     gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.spacing ? "mt-9" : "mt-2"}`}>
                         <span className="text-2xl block float-left">
                             {menu.icon ? menu.icon : <RiDashboardFill/>}
@@ -63,7 +62,7 @@ export default function SideNav() {
                     {menu.submenu && submenuOpen && open && (
                         <ul>
                             {menu.submenuItems.map((submenuItem, index) => (
-                                <li key={index} className="text-gray-300 text-sm flex items-center gap-x-4
+                                <li key={index} className="text-white mt-2 text-sm flex items-center gap-x-4
                                 cursor-pointer p-2 px-5 hover:bg-light-white rounded-md">
                                     {submenuItem.title}
                                 </li>
