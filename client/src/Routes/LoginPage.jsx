153c4f3d3a761f1ff1e2f1be334b29ac5f76ab56
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-30 14:45:09 -0400

    Readme Updated

---
+++ client/src/Routes/LoginPage.jsx
@@ -0,0 +1,17 @@
+import LoginForm from "../Components/LoginForm";
+
+
+export default function LoginPage() {
+    return(
+        <div className="flex w-full h-screen">
+            <div className="w-full flex items-center justify-center lg:w-1/2">
+                <LoginForm />
+            </div>
+            <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+                <div className="w-60 h-60 bg-gradient-to-tr from-blue-800 to-green-400 rounded-full animate-bounce"></div>
+                <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" > 
+                </div>
+            </div>
+        </div>
+    )
+}
