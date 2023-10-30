dea497c66f06e850fd20aa4e13ddc3f73523e61b
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-30 18:07:41 -0400

    Dashboard UI updates Images and SVGs

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -1,3 +1,7 @@
+import PlusSignSVG from "../assets/plus-sign-svgrepo-com.svg"
+import LogoutSVG from "../assets/logout-svgrepo-com.svg"
+import SettingsSVG from "../assets/settings-svgrepo-com.svg"
+
 export default function Dashboard() {
     return (
         <div className="w-full min-h-screen flex items-center justify-center">
@@ -6,47 +10,53 @@ export default function Dashboard() {
       <div className="flex justify-between items-center">
         <h3 className="text-3xl font-extralight text-white/50">Dashboard</h3>
         <div className="inline-flex items-center space-x-2">
-          <a className="bg-gray-900 text-white/50 p-2 rounded-md hover:text-white smooth-hover" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            </svg>
+          <a className="bg-gray-200 text-white/50 p-2 rounded-md hover:text-white smooth-hover" href="#">
+          <img
+            src={SettingsSVG} // Replace with the actual URL or path to your SVG file
+            alt="plus sign to add clients" // Provide an appropriate alt text for accessibility
+            className="h-6 w-6"/>
           </a>
-          <a className="bg-gray-900 text-white/50 p-2 rounded-md hover:text-white smooth-hover" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            </svg>
+          <a className="bg-gray-200 text-white/50 p-2 rounded-md hover:text-white smooth-hover" href="#">
+          <img
+            src={LogoutSVG} // Replace with the actual URL or path to your SVG file
+            alt="plus sign to add clients" // Provide an appropriate alt text for accessibility
+            className="h-6 w-6"/>
           </a>
         </div>
       </div>
       <div className="mb-10 sm:mb-0 mt-10 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <div className="group bg-gray-900/30 py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/40 hover:smooth-hover">
           <a className="bg-gray-900/70 text-white/50 group-hover:text-white group-hover:smooth-hover flex w-20 h-20 rounded-full items-center justify-center" href="#">
-            <svg xmlns="" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            </svg>
+          <img
+            src={PlusSignSVG} // Replace with the actual URL or path to your SVG file
+            alt="plus sign to add clients" // Provide an appropriate alt text for accessibility
+            className="h-7 w-10"/>
           </a>
           <a className="text-white/50 group-hover:text-white group-hover:smooth-hover text-center" href="#">NEW CLIENT</a>
         </div>
         <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-          <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1547592180-85f173990554?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="cuisine" />
+          <img className="w-20 h-20 object-cover object-center rounded-full" src="https://logoipsum.com/logoipsum.png" alt="cuisine" />
           <h4 className="text-white text-2xl font-bold capitalize text-center"> ALL CLIENTS</h4>
           <p className="text-white/50">VIEW ALL 55 CLIENTS </p>
         </div>
         <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-          <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1513364776144-60967b0f800f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80" alt="art" />
+        <img className="w-20 h-20 object-cover object-center rounded-full" src="https://logoipsum.com/logoipsum.png" alt="cuisine" />
           <h4 className="text-white text-2xl font-bold capitalize text-center">DOCUMENTS</h4>
           <p className="text-white/50">GENERATE CLIENT DOCS</p>
         </div>
         <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-          <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1560419015-7c427e8ae5ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="gaming" />
+        <img className="w-20 h-20 object-cover object-center rounded-full" src="https://logoipsum.com/logoipsum.png" alt="cuisine" />
           <h4 className="text-white text-2xl font-bold capitalize text-center">EDIT CLIENT</h4>
           <p className="text-white/50">UPDATE CLIENT INFO</p>
         </div>
         <div></div>
         <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-          <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80" alt="cinema" />
+        <img className="w-20 h-20 object-cover object-center rounded-full" src="https://logoipsum.com/logoipsum.png" alt="cuisine" />
           <h4 className="text-white text-2xl font-bold capitalize text-center">OPEN CASES</h4>
           <p className="text-white/50">VIEW PENDING CASES</p>
         </div>
         <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-          <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80" alt="cinema" />
+        <img className="w-20 h-20 object-cover object-center rounded-full" src="https://logoipsum.com/logoipsum.png" alt="cuisine" />
           <h4 className="text-white text-2xl font-bold capitalize text-center">ARCHIVED CASES</h4>
           <p className="text-white/50"> VIEW CLOSED CASES</p>
         </div>
