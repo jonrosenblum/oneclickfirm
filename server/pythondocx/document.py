31760719ed430d81dc195a6f69336ec274f802bb
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-31 17:44:34 -0400

    MSWORD to PDF Update / MSWord opening up

--- server/pythondocx/document.py
+++ server/pythondocx/document.py
@@ -1,7 +1,6 @@
 from docxtpl import DocxTemplate
 from docx2pdf import convert
-import random
-from io import BytesIO
+import os
 
 # Load the Word document template
 discovery_template_path = "./templates/discoveryTemplate.docx"
@@ -14,8 +13,8 @@ retainer_doc = DocxTemplate(retainer_template_path)
 
 # Define a context dictionary with placeholders and their corresponding random values
 context = {
-    'fax_number': "(732)-726-2361",  
-    'todays_date': "October, 31st 2023",  
+    'fax_number': "(732)-726-2361",
+    'todays_date': "October, 31st 2023",
     'court_house_name': "Woodbridge Municipal Court",
     'court_house_street': "1 Main Street",
     'court_house_city': "Woodbridge",
@@ -32,30 +31,34 @@ discovery_doc.render(context)
 representation_doc.render(context)
 retainer_doc.render(context)
 
-# Define the output file paths for both DOCX and PDF files
+# Define the output folder for both DOCX and PDF files
 output_folder = "templates/ClientDocuments/"
-client_name = context['client_name']
 
-discovery_output_path_docx = f"{output_folder}{client_name}_discovery.docx"
-representation_output_path_docx = f"{output_folder}{client_name}_representation.docx"
-retainer_output_path_docx = f"{output_folder}{client_name}_retainer.docx"
-discovery_output_path_pdf = f"{output_folder}{client_name}_discovery.pdf"
-representation_output_path_pdf = f"{output_folder}{client_name}_representation.pdf"
-retainer_output_path_pdf = f"{output_folder}{client_name}_retainer.pdf"
+# Create a subfolder with the client's name
+client_name = context['client_name']
+client_folder = os.path.join(output_folder, client_name)
+
+# Ensure the client folder exists, or create it if not
+os.makedirs(client_folder, exist_ok=True)
+
+# Define the output file paths for DOCX and PDF files inside the client's folder
+discovery_output_path_docx = os.path.join(client_folder, f"{client_name}_discovery.docx")
+representation_output_path_docx = os.path.join(client_folder, f"{client_name}_representation.docx")
+retainer_output_path_docx = os.path.join(client_folder, f"{client_name}_retainer.docx")
+discovery_output_path_pdf = os.path.join(client_folder, f"{client_name}_discovery.pdf")
+representation_output_path_pdf = os.path.join(client_folder, f"{client_name}_representation.pdf")
+retainer_output_path_pdf = os.path.join(client_folder, f"{client_name}_retainer.pdf")
 
 # Save the filled-in DOCX files
 discovery_doc.save(discovery_output_path_docx)
 representation_doc.save(representation_output_path_docx)
 retainer_doc.save(retainer_output_path_docx)
 
-# Convert the Word documents to PDF
-convert(discovery_output_path_docx)
-convert(representation_output_path_docx)
-convert(retainer_output_path_docx)
+# Define a function to convert DOCX to PDF using reportlab
+def convert_to_pdf(docx_path, pdf_path):
+    convert(docx_path, pdf_path)
 
-# Rename the converted PDF files to match the naming convention
-import os
-
-os.rename(f"{discovery_output_path_docx}.pdf", discovery_output_path_pdf)
-os.rename(f"{representation_output_path_docx}.pdf", representation_output_path_pdf)
-os.rename(f"{retainer_output_path_docx}.pdf", retainer_output_path_pdf)
+# Convert the Word documents to PDF using reportlab
+convert_to_pdf(discovery_output_path_docx, discovery_output_path_pdf)
+convert_to_pdf(representation_output_path_docx, representation_output_path_pdf)
+convert_to_pdf(retainer_output_path_docx, retainer_output_path_pdf)
