fb98ed556739c08496103ac267beb4131998a31c
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-30 22:41:39 -0400

    Add New Client Modal

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -4,9 +4,22 @@ import DocumentsPNG from "../assets/documents-svgrepo-com.png"
 import EditPNG from "../assets/edit-svgrepo-com.png"
 import CompletePNG from "../assets/checkmark-complete-correct-svgrepo-com.png"
 import PendingPNG from "../assets/shifts-pending-svgrepo-com.png"
+import { useState } from "react"
 
 
 export default function Dashboard() {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const openModal = () => {
+    setIsModalVisible(true);
+
+  const modalClose = () => {
+    setIsModalVisible(false);
+  }
+
+
+  }
     return (
         <div className="bg-gray-900/20 w-full min-h-screen flex items-center justify-center">
   <div className="font-oswald bg-gradient-to-tr from-blue-800 to-green-400 flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
@@ -23,7 +36,7 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="mb-10 sm:mb-0 mt-10 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <div className="group bg-gray-900/30 py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/40 hover:smooth-hover">
+        <div onClick={openModal} className="group bg-gray-900/30 py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/40 hover:smooth-hover">
           <a className="bg-gray-900/70 text-white/50 group-hover:text-white group-hover:smooth-hover flex w-20 h-20 rounded-full items-center justify-center" href="#">
           <img
             src={PlusSignSVG}
@@ -31,6 +44,46 @@ export default function Dashboard() {
             className="h-7 w-10"/>
           </a>
           <a className="text-white/50 group-hover:text-white group-hover:smooth-hover text-center" href="#">NEW CLIENT</a>
+
+
+          {isModalVisible ? (
+                    <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <div className="modal-overlay absolute w-full h-full backdrop-blur-md" />
+                    <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 p-4">
+                      <div className="modal-content py-4 text-left px-6 mb-5">
+                        <h1 className="text-2xl font-bold mb-4">Add New Client</h1>
+                        <p>Add new client to generate documents</p>
+                        <div>
+                    <label className="text-lg font-medium">Client Name</label>
+                    <input
+                    className="w-full border-2 border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
+                    placeholder="e.g. John Smith"/>
+                </div>
+                <div className="mt-2">
+                    <label className="text-lg font-medium">Date of Violation</label>
+                    <input
+                    className="w-full border-2 border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
+                    type="date"/>
+                </div>
+                <div className="mt-2">
+                    <label className="text-lg font-medium">Violation Number or Ticker Number</label>
+                    <input
+                    className="w-full border-2 border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
+                    placeholder="e.g. 1217E23006116 "
+                    type="text"/>
+                </div>
+                      </div>
+              
+                        
+                        <button className="modal-action-button px-4 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-700">
+                          Save
+                        </button>
+                      </div>
+                    </div>
+    
+                ) : null}
+          
+          
         </div>
         <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
           <img className="w-20 h-20 object-cover object-center rounded-full" src={AllClientsPNG} alt="all clients" />
