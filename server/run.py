3bee809b1bcc3467ee37eea067d069d3f32ab7b6
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-06 02:04:32 -0500

    Fix: setting host through env

--- server/run.py
+++ server/run.py
@@ -2,4 +2,4 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    app.run(debug=os.environ.get('DEBUG') if "DEBUG" in os.environ.keys() else False,port=os.environ.get('PORT'))
+    app.run(debug=os.environ.get('DEBUG') if "DEBUG" in os.environ.keys() else False,port=os.environ.get('PORT'),host=os.environ.get('HOST') if 'HOST' in os.environ.keys() else 'http://localhost')
