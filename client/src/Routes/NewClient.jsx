73b021d2ab2134dd7d60f87be230d196dbae2b50
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-11 17:55:15 -0500

    JSON RECIEVED FOR CLIENT DATA / DO NOT HAVE TO ENTER INTO DB YET

--- client/src/Routes/NewClient.jsx
+++ client/src/Routes/NewClient.jsx
@@ -1,25 +1,19 @@
-import { BiArrowBack } from "react-icons/bi";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import ClientLegalInformation from "../Components/Pieces/ClientLegalInformation";
-import ClientAddConfirmInformation from "../Components/Pieces/ClientAddConfirmInformation";
 import axiosInstance from "../axios";
 
 export default function NewClient() {
   const [formData, setFormData] = useState({
     client_name: "",
     violation_date: "",
-    violation_ticket_number: "",
   });
   const formRef = useRef(null);
   const [formSubmitted, setFormSubmitted] = useState(false);
-
-
-  const navigate = useNavigate();
+  const [responseData, setResponseData] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(formData);
     setFormSubmitted(true);
 
     try {
@@ -31,6 +25,7 @@ export default function NewClient() {
 
       if (response.status === 200) {
         console.log("Request Success");
+        setResponseData(response.data.data);
       } else {
         console.error("Request Failed");
       }
@@ -39,69 +34,61 @@ export default function NewClient() {
     }
   };
 
-    return (
+  return (
+    <div className="bg-gray-100 w-full min-h-screen flex justify-center">
+      <div className="bg-white font-oswald flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:my-2 sm:mx-4 sm:rounded-2xl pt-4 h-[100%]">
+        <div className="bg-gray-200 flex-1 flex-col px-2 sm:px-0">
 
-      <div className="bg-gradient-to-tl from-blue-800 to-green-400 w-full min-h-screen flex items-center justify-center">
-        <div className="bg-gradient-to-tr from-blue-800 to-green-400 font-oswald flex-1 flex flex-col justify-center space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
-          <div className="search-client flex flex-col justify-center items-center">
-            <h1 className="text-3xl text-white">SEARCH FOR CLIENTS</h1>
-            <p className="mt-5 mb-5 text-gray-300">Please use the search form to search for a client</p>
+          <div className="flex justify-between p-2">
+            <h1 className="title text-2xl">Add new client</h1>
+            <button className="button rounded-md bg-gradient-to-r from-s via-cyan-500 to-emerald-500 text-xs p-2">View all</button>
+          </div>
 
+          <div>
             <form ref={formRef} onSubmit={handleSubmit}>
               <div>
-                <label className="font-medium text-white">Client Name:</label>
-                <input className="w-full border-2 border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
+                <label className="">Legal Name</label>
+                <input className=""
                 placeholder="e.g. John Smith"
                 type="text"
                 onChange={(e)=> setFormData({...formData, client_name: e.target.value})}/>
               </div>
-              <div className="mt-4">
-                <label className="font-medium text-white">Date of Violation:</label>
-                <input className="w-full border-2 border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
+              <div className="">
+                <label className="">Violation Date</label>
+                <input className=""
                 type="date"
                 onChange={(e)=> setFormData({...formData, violation_date: e.target.value})}/>
               </div>
-              <div className="mt-4">
-                <label className="font-medium text-white">Violation or Ticket Number:</label>
-                <input className="w-full border-2 border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
-                type="text"
-                placeholder="e.g. 1104E23004259"
-                onChange={(e)=> setFormData({...formData, violation_ticket_number: e.target.value})}/>
-              </div>
-              <div className="mt-5 flex justify-between">
-                <button onClick={()=> {navigate("/home")}} className="modal-action-button px-4 py-2 bg-white text-black font-bold rounded-md hover:bg-blue-300 focus:outline-none focus:shadow-outline-blue active:bg-blue-500">
-                  <span><BiArrowBack/></span>
-                </button>
-                <button className="modal-action-button px-4 py-2 bg-white text-black font-bold rounded-md hover:bg-blue-300 focus:outline-none focus:shadow-outline-blue active:bg-blue-500">
+              <button className="modal-action-button px-4 py-2 bg-white text-black font-bold rounded-md hover:bg-blue-300 focus:outline-none focus:shadow-outline-blue active:bg-blue-500">
                   <span><AiOutlineSearch/></span>
                 </button>
-              </div>
+              <button
+                className="modal-action-button px-4 py-2 bg-white text-black font-bold rounded-md hover:bg-blue-300 focus:outline-none focus:shadow-outline-blue active:bg-blue-500"
+              >
+                <span><AiOutlineSearch /></span>
+              </button>
             </form>
           </div>
 
-          <div className="mb-10 sm:mb-0 mt-5 grid grid-cols-2 gap-4">
-            <div className='flex flex-col justify-center'>
-                  {formSubmitted ? <ClientLegalInformation/> : (
-                     <div className="p-10 text-center h-1/2 bg-gradient-to-tr from-blue-800 to-green-400 ml-6 rounded-xl">
-                      <p className="animate-bounce text-white font-medium">READY TO GENERATE DOCUMENTS?</p>
-                    </div>
-                  )}
+          {formSubmitted && responseData && (
+            <div>
+              {/* Display the received data */}
+              <p>Client ID: {responseData.client_id}</p>
+              <p>Client Name: {responseData.client_info.client_name}</p>
+              <p>Client Age: {responseData.client_info.client_age}</p>
+              <p>Client Birth Place: {responseData.client_info.client_birth_place}</p>
+              <p>Court House Name: {responseData.court_info.court_house_name}</p>
+              <p>Court House Street: {responseData.court_info.court_house_street}</p>
+              <p>Court House City: {responseData.court_info.court_house_city}</p>
+              <p>Court House State: {responseData.court_info.court_house_state}</p>
+              <p>Court House Zip: {responseData.court_info.court_house_zip}</p>
+              <p>Phone Number: {responseData.court_info.phone_number}</p>
+              <p>Fax Number: {responseData.court_info.fax_number}</p>
+              <p>Violations: {responseData.violations.join(", ")}</p>
             </div>
-
-            <div className='flex flex-col justify-center'>
-            {formSubmitted ? <ClientAddConfirmInformation/> : (
-                    <div className="p-10 text-center h-1/2 bg-gradient-to-tr from-blue-800 to-green-400 ml-6 rounded-xl">
-                    <p className="animate-bounce text-white font-medium">JUST A FEW CLICKS AWAY...</p>
-                  </div>
-                  )}
-            </div>
-
-          </div>
+          )}
         </div>
       </div>
-  
-
-    );
+    </div>
+  );
 }
-
-    
