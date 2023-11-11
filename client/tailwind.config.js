547d60cb0f833cadedfca02bf88c017b1470736c
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-11 04:38:25 -0500

    TAILWIND CONFIG Updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,8 +12,20 @@ export default {
       colors: {
         "dark-purple": "#081A51",
         "light-white": "rgba(255, 255, 255, 0.18)",
-      }
+      },
+
+      gradientColorStops: {
+        s: "#2CB3A5",
+        s1:"#13A990",
+        s2:"#3F83E6",
+        s3:"#3F83E6"
+
     },
+    backgroundColor:{
+      hov:"#5EB9CC"
+    }
+
   },
   plugins: [],
+}
 }
