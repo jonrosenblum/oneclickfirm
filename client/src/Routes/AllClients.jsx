308f9a26e9a0ab5bf49f630242a383dd612d36aa
Naeem113 <na6574113@gmail.com>
2023-11-15 00:36:58 +0500

    update the design for all clients table

--- client/src/Routes/AllClients.jsx
+++ client/src/Routes/AllClients.jsx
@@ -70,6 +70,7 @@ export default function AllClients() {
   );
 
   return (
+    <>
     <div>
       {clientInfo.length === 0 ? (
         <div className="bg-gradient-to-tr font-oswald from-blue-800 to-green-400 w-full min-h-screen flex items-center justify-center">
@@ -109,7 +110,7 @@ export default function AllClients() {
             </div>
             <div className="block w-full overflow-x-auto max-h-[800px] overflow-y-auto">
               <div className="">
-                <table className="table-auto items-center w-full bg-transparent border-collapse">
+                {/* <table className="table-auto items-center w-full bg-transparent border-collapse">
                   <thead className="sticky top-0">
                     <tr>
                       <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
@@ -208,7 +209,118 @@ export default function AllClients() {
                       </tr>
                     ))}
                   </tbody>
-                </table>
+                </table> */}
+                <table className="w-full text-sm text-left">
+              <thead className="text-xs text-white  rounded-lg uppercase bg-gradient-to-b from-[#30b5b1] to-blue-500">
+                <tr>
+                  <th scope="col" className="px-6 rounded-tl-lg py-3">
+                  Client Name
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                  Case Status
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                  Court House
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                  Country
+                  </th>
+                  <th scope="col" className="px-6 py-3 rounded-tr-lg">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+              {filteredClients.map((client) => (
+                <tr key={client.client_id} className="bg-white hover:bg-gray-50">
+                  <th
+                    scope="row"
+                    className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    <img
+                      className="w-10 h-10 rounded-full"
+                      src="https://demos.creative-tim.com/notus-js/assets/img/bootstrap.jpg"
+                      alt="Jese image "
+                    />
+                    <div className="ps-3">
+                      <div className="text-base font-semibold">
+                      {client.client_name}
+                      </div>
+                      <div className="font-normal text-gray-500">
+                      {client.client_id}
+                      </div>
+                    </div>
+                  </th>
+                  <td className="px-6 py-4">
+                    <div className="flex items-center">
+                      { client.case_status === "OPEN" && <><div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> <span>Online</span></>}
+                      { client.case_status != "OPEN" && <><div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> <span>Offline</span></>}
+                    </div>
+                  </td>
+                  <td className="px-6 py-4">{client.court_house_name.toUpperCase()}</td>
+                  <td className="px-6 py-4">{client.court_house_county.toUpperCase()}</td>
+
+                  <td className="px-6 py-4">
+                    <button
+                      id="dropdownDefaultButton"
+                      data-dropdown-toggle="dropdown"
+                      className="  text-sm text-left inline-flex items-center font-medium relative text-blue-600 dark:text-blue-500 hover:underline"
+                      type="button"
+                    >
+                      Action
+                      {/* <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+</svg> */}
+                    </button>
+
+                    <div
+                      id="dropdown"
+                      className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-40"
+                    >
+                      <ul
+                        className="py-2 text-sm text-gray-700 "
+                        aria-labelledby="dropdownDefaultButton"
+                      >
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600"
+                          >
+                            Dashboard
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600"
+                          >
+                            Settings
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600"
+                          >
+                            Earnings
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="javascript:;"
+                            onClick={() => downloadDocuments(client)}
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600"
+                          >
+                            Download Documents
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
               </div>
             </div>
           </div>
@@ -218,5 +330,6 @@ export default function AllClients() {
         <AlertDocumentDownload onClose={() => setShowAlert(false)} />
       )}
     </div>
+    </>
   );
 }
