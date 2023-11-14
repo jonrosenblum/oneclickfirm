f7055d0b2c7e06551b48c8be5daaa7edd53e28a7
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-14 18:56:45 -0500

    Updates to UI for All Clients

--- client/src/Routes/AllClients.jsx
+++ client/src/Routes/AllClients.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "./../axios";
 import { useNavigate } from "react-router-dom";
-// import { AiOutlineDownload } from 'react-icons/ai';
 import AlertDocumentDownload from "../Components/Pieces/AlertDocumentDownload";
 
 export default function AllClients() {
@@ -9,6 +8,7 @@ export default function AllClients() {
   const [searchInput, setSearchInput] = useState(""); // State variable for search input
   const navigate = useNavigate();
   const [showAlert, setShowAlert] = useState(false); // State variable to control the alert visibility
+  const [dropdownOpen, setDropdownOpen] = useState({});
 
   useEffect(() => {
     // Fetch client information from your backend
@@ -69,7 +69,18 @@ export default function AllClients() {
       client.court_house_name.toLowerCase().includes(searchInput.toLowerCase())
   );
 
+
+  const toggleDropdown = (clientId) => {
+    setDropdownOpen((prevState) => ({
+      ...prevState,
+      [clientId]: !prevState[clientId],
+    }));
+  };
+
+ 
+
   return (
+    <>
     <div>
       {clientInfo.length === 0 ? (
         <div className="bg-gradient-to-tr font-oswald from-blue-800 to-green-400 w-full min-h-screen flex items-center justify-center">
@@ -89,126 +100,128 @@ export default function AllClients() {
           </div>
         </div>
       ) : (
-        <div className="bg-gray-200 w-full p-4 min-h-screen flex">
-          <div className="relative flex flex-col min-w-0 break-words w-full shadow-lg rounded-lg bg-gray-900 text-white">
+        <div className="bg-white w-full p-4 min-h-screen flex">
+          <div className="relative flex flex-col min-w-0 break-words w-full shadow-lg rounded-lg bg-gray-200  text-gray-900">
             <div className="rounded-t mb-0 px-4">
               <div className="flex flex-wrap">
-                <div className="p-4 relative w-full max-w-full flex-grow flex-1">
-                  <h3 className="title text-2xl font-bold">All Clients</h3>
-                  <div className="mb-4">
-                    <input
-                      type="text"
-                      placeholder="Search for clients"
-                      value={searchInput}
-                      onChange={(e) => setSearchInput(e.target.value)}
-                      className="p-2 w-1/6 mt-4 text-sm text-black border border-gray-300 rounded"
-                    />
-                  </div>
-                </div>
+              <div className="p-4 flex justify-between relative w-full max-w-full flex-grow flex-1">
+                <h3 className="title text-2xl font-bold">All Clients</h3>
+                <input
+                  type="text"
+                  placeholder="Search for clients"
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  className="p-2  text-sm text-black border border-gray-300 rounded"
+                />
+              </div>
               </div>
             </div>
             <div className="block w-full overflow-x-auto max-h-[800px] overflow-y-auto">
-              <div className="">
-                <table className="table-auto items-center w-full bg-transparent border-collapse">
-                  <thead className="sticky top-0">
-                    <tr>
-                      <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
-                        Client Name
-                      </th>
-                      <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
-                        Client ID
-                      </th>
-                      <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
-                        Case Status
-                      </th>
-                      <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
-                        Client Documents
-                      </th>
-                      <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
-                        Court House{" "}
-                      </th>
-                      <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
-                        County{" "}
-                      </th>
-                      <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-gray-800 text-gray-300 border-gray-700">
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {filteredClients.map((client) => (
-                      <tr
-                        key={client.client_id}
-                        className="hover:bg-gray-800 transition duration-150 ease-in-out"
+              <div className="p-4">
+                <table className="w-full text-sm text-left">
+              <thead className="text-xs text-white  rounded-lg uppercase bg-gradient-to-b from-[#30b5b1] to-blue-500">
+                <tr>
+                  <th scope="col" className="px-6 rounded-tl-lg py-3">
+                  Client Name
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                  Case Status
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                  Court House
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                  County
+                  </th>
+                  <th scope="col" className="px-6 py-3 rounded-tr-lg">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+              {filteredClients.map((client) => (
+                <tr key={client.client_id} className="bg-white hover:bg-gray-50">
+                  <th
+                    scope="row"
+                    className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    <img
+                      className="w-10 h-10 rounded-full"
+                      src="https://demos.creative-tim.com/notus-js/assets/img/bootstrap.jpg"
+                      alt="Jese image "
+                    />
+                    <div className="ps-3">
+                      <div className="text-base font-semibold">
+                      {client.client_name}
+                      </div>
+                      <div className="font-normal text-gray-500">
+                      {client.client_id}
+                      </div>
+                    </div>
+                  </th>
+                  <td className="px-6 py-4">
+                    <div className="flex items-center">
+                      { client.case_status === "OPEN" && <><div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> <span>{client.case_status}</span></>}
+                      { client.case_status != "OPEN" && <><div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> <span>{client.case_status}</span></>}
+                    </div>
+                  </td>
+                  <td className="px-6 py-4">{client.court_house_name.toUpperCase()}</td>
+                  <td className="px-6 py-4">{client.court_house_county.toUpperCase()}</td>
+
+                  <td className="px-6 py-4">
+                    <button
+                      onClick={() => toggleDropdown(client.client_id)}
+                      className="text-sm text-left inline-flex items-center font-medium relative text-blue-600 dark:text-blue-500 hover:underline"
+                      type="button"
+                    >
+                      View actions
+                    </button>
+
+                    <div
+                       className={`${
+                        dropdownOpen[client.client_id] ? "" : "hidden"
+                      } z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-40`}
+                    >
+                      <ul
+                        className="py-2 text-sm text-gray-700 "
+                        aria-labelledby={`dropdownDefaultButton_${client.client_id}`}
                       >
-                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-s whitespace-nowrap p-4 text-left flex items-center">
-                          <img
-                            src="https://demos.creative-tim.com/notus-js/assets/img/bootstrap.jpg"
-                            className="h-12 w-12 bg-white rounded-full border"
-                            alt="Client Avatar"
-                          />
-                          <span className="ml-3 font-bold">
-                            {client.client_name}
-                          </span>
-                        </td>
-                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                          {client.client_id}
-                        </td>
-                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                          <i
-                            className={`fas fa-circle ${
-                              client.case_status === "OPEN"
-                                ? "text-green-500"
-                                : "text-red-500"
-                            } mr-2`}
+                        <li>
+                          <a
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
                           >
-                            {client.case_status}
-                          </i>
-                        </td>
-                        <td>
-                          <div className="flex">
-                            <img
-                              src="https://png.pngtree.com/png-vector/20190406/ourmid/pngtree-doc-file-document-icon-png-image_913809.jpg"
-                              alt="..."
-                              className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow "
-                            />
-                            <img
-                              src="https://png.pngtree.com/png-vector/20190406/ourmid/pngtree-doc-file-document-icon-png-image_913809.jpg"
-                              alt="..."
-                              className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
-                            />
-                            <img
-                              src="https://png.pngtree.com/png-vector/20190406/ourmid/pngtree-doc-file-document-icon-png-image_913809.jpg"
-                              alt="..."
-                              className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
-                            />
-                            <img
-                              src="https://png.pngtree.com/png-vector/20190406/ourmid/pngtree-doc-file-document-icon-png-image_913809.jpg"
-                              alt="..."
-                              className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
-                            />
-                          </div>
-                        </td>
-                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                          {client.court_house_name.toUpperCase()}
-                        </td>
-                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                          {client.court_house_county.toUpperCase()}
-                        </td>
-                        <td className="border-t-0 px-2 align-middle items-center border-l-0 border-r-0 text-s whitespace-nowrap">
-                          <div>
-                            <button
-                              onClick={() => downloadDocuments(client)}
-                              className="rounded-md bg-blue-500 p-2 text-xs cursor-pointer button active:scale-[.95] active:duration-75 hover:scale-[1.05] ease-in-out transition-all"
-                            >
-                              Download Documents
-                            </button>
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
+                            Dashboard
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
+                          >
+                            Settings
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
+                          >
+                            Earnings
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            onClick={() => downloadDocuments(client)}
+                            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
+                          >
+                            Download Documents
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
               </div>
             </div>
           </div>
@@ -218,5 +231,6 @@ export default function AllClients() {
         <AlertDocumentDownload onClose={() => setShowAlert(false)} />
       )}
     </div>
+    </>
   );
 }
