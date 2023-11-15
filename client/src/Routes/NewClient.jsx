10c6065870b525cde5383c38332d106fc5eb678f
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-15 11:17:16 -0500

    NEW CLIENT FORM UPDATES

--- client/src/Routes/NewClient.jsx
+++ client/src/Routes/NewClient.jsx
@@ -1,6 +1,9 @@
 import { AiOutlineSearch } from "react-icons/ai";
 import { useEffect, useRef, useState } from "react";
 import axiosInstance from "../axios";
+import ReactCreditCard from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
+import creditCardType from 'credit-card-type';
 
 export default function NewClient() {
   const [searchData, setsearchData] = useState({
@@ -57,6 +60,18 @@ export default function NewClient() {
   };
 
 
+  const handleCreditCardNumberChange = (e) => {
+    const { value } = e.target;
+    const updatedForm = {
+      ...newClientForm,
+      credit_card_number: value,
+      // Identify card type based on the input credit card number
+      credit_card_type: creditCardType(value)[0]?.niceType || '', // Update the card type directly
+    };
+    setnewClientForm(updatedForm);
+  };
+  
+
   useEffect(() => {
     if (responseData && responseData.client_info && responseData.court_info) {
       setnewClientForm((prevForm) => ({
@@ -105,6 +120,30 @@ export default function NewClient() {
     }
   };
 
+
+  const handleClearForm = () => {
+    setnewClientForm({
+      client_name: responseData?.client_info?.client_name ?? "",
+      todays_date: today,
+      fax_number: responseData?.court_info?.fax_number ?? "",
+      complaint_violation_ticket_numbers: "",
+      court_house_name: responseData?.court_info?.court_house_name ?? "",
+      court_house_address: responseData?.court_info?.court_house_street ?? "",
+      court_house_city: responseData?.court_info?.court_house_city ?? "",
+      court_house_state: responseData?.court_info?.court_house_state ?? "",
+      court_house_zip: responseData?.court_info?.court_house_zip ?? "",
+      court_house_county: responseData?.court_info?.court_house_county ?? "",
+      incident_date: searchData.violation_date,
+      case_status: "OPEN",
+      dwi_status: "",
+      credit_card_number: "",
+      credit_card_expiration: "",
+      credit_card_cvv: "",
+      client_balance: "",
+      payment_type: "",
+      credit_card_type: "",
+    });
+  };
   return (
     <div className="bg-gray-100 w-full min-h-screen flex justify-center">
       <div className="bg-white font-oswald flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:my-2 sm:mx-4 sm:rounded-2xl pt-4 h-[100%]">
@@ -143,7 +182,7 @@ export default function NewClient() {
           )}
 
           {searchFormSubmitted && responseData && !loading && (
-            <div className="p-5">
+            <div className="p-5 ">
               <div className="m-2">
                 <h1 className="text-2xl">Client Legal Information</h1>
               </div>
@@ -202,104 +241,118 @@ export default function NewClient() {
                   <div className="bg-gray-300 p-5 m-2">
                       <h1 className="text-lg">Payment Information</h1>
                       <div className="m-2">
+                        <label className="m-2">Payment Type</label>
+                        <select
+                          className="rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                          value={newClientForm.payment_type}
+                          onChange={(e)=>{setnewClientForm({...newClientForm, payment_type: e.target.value})}}
+                          required
+                          >
+                          <option value="">Select Payment Type</option>
+                          <option value="Credit Card">Credit Card</option>
+                          <option value="Zelle">Zelle</option>
+                        </select>
 
-                      <label>Payment Type</label>
-                      <select
-                        value={newClientForm.payment_type}
-                        onChange={(e) => {
-                          setnewClientForm({
-                            ...newClientForm,
-                            payment_type: e.target.value,
-                          });
-                        }}
-                        required
-                        >
-                        <option value="">Select Payment Type</option>
-                        <option value="Credit Card">Credit Card</option>
-                        <option value="Zelle">Zelle</option>
-                      </select>
+                        
 
-                      <label>Card Type</label>
-                      <select
-                        required
-                        value={newClientForm.credit_card_type}
-                        onChange={(e) => {
-                          setnewClientForm({
-                            ...newClientForm,
-                            credit_card_type: e.target.value,
-                          });
-                        }}
-                        disabled={newClientForm.payment_type === "Zelle"}
-                        >
-                        <option value="">Select Card Type</option>
-                        <option value="American Express">American Express</option>
-                        <option value="Visa">Visa</option>
-                        <option value="MasterCard">MasterCard</option>
-                        <option value="Discover">Discover</option>
-                      </select>
+                        {newClientForm.payment_type === 'Credit Card' && (
+                          <div className="flex justify-between">
+                                    <div className="flex gap-4 items-center m-2">
+                                      <div className="flex flex-col">
+                                        <label className="m-2">Credit Card Number</label>
+                                        <input
+                                          className="rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                                          type="text"
+                                          placeholder="Credit Card Number"
+                                          value={newClientForm.credit_card_number}
+                                          required
+                                          onChange={handleCreditCardNumberChange}
+                                        />
+                                        <label className="m-2">Expiration</label>
+                                          <input
+                                            className="rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                                            type="text"
+                                            placeholder="MM/YY"
+                                            value={newClientForm.credit_card_expiration}
+                                            required
+                                            onChange={(e) => {
+                                              setnewClientForm({
+                                                ...newClientForm,
+                                                credit_card_expiration: e.target.value,
+                                              });
+                                            }}
+                                          
+                                            />
+                                      </div>
+                                      <div className="flex flex-col">
+                                            <label className="m-2">CVV</label>
+                                            <input
+                                              className="rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                                              type="text"
+                                              placeholder="XXXX"
+                                              value={newClientForm.credit_card_cvv}
+                                              required
+                                              onChange={(e) => {
+                                                setnewClientForm({
+                                                  ...newClientForm,
+                                                  credit_card_cvv: e.target.value,
+                                                });
+                                              }}
+                                              />
+                                            <label className="m-2">Client Balance</label>
+                                            <input
+                                              className="rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                                              type="text"
+                                              placeholder="$"
+                                              value={newClientForm.client_balance}
+                                              required
+                                              onChange={(e) => {
+                                                setnewClientForm({
+                                                  ...newClientForm,
+                                                  client_balance: e.target.value,
+                                                });
+                                              }}
+                                            />
+                                        
+                                      </div>
 
-                      <label>Credit Card Number</label>
-                      <input
-                        type="text"
-                        placeholder="Credit Card Number"
-                        value={searchData.credit_card_number}
-                        required
-                        onChange={(e) => {
-                          setnewClientForm({
-                            ...newClientForm,
-                            credit_card_number: e.target.value,
-                          });
-                        }}
-                        disabled={newClientForm.payment_type === "Zelle"}
-                        />
+                                    
+                                      </div>
+                                      <div>
+                                         <ReactCreditCard
+                                            cvc={newClientForm.credit_card_cvv}
+                                            expiry={newClientForm.credit_card_expiration}
+                                            name={newClientForm.client_name}
+                                            number={newClientForm.credit_card_number}/>
+                                      </div>
+                                    </div>
+                                  )}
 
-                      <label>Expiration</label>
-                      <input
-                        type="text"
-                        placeholder="MM/YY"
-                        value={newClientForm.credit_card_expiration}
-                        required
-                        onChange={(e) => {
-                          setnewClientForm({
-                            ...newClientForm,
-                            credit_card_expiration: e.target.value,
-                          });
-                        }}
-                        disabled={newClientForm.payment_type === "Zelle"}
-                        />
+                        {newClientForm.payment_type === 'Zelle' && (
+                                    <>
+                                      <label className="m-2">Client Balance</label>
+                                      <input
+                                        className="rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                                        type="text"
+                                        placeholder="$"
+                                        value={newClientForm.client_balance}
+                                        required
+                                        onChange={(e) => {
+                                          setnewClientForm({
+                                            ...newClientForm,
+                                            client_balance: e.target.value,
+                                          });
+                                        }}
+                                      />
+                                    </>
+                                  )}
+                      </div>
+                      <div className="flex mt-4 justify-between">
+                        <button className="bg-red-500 rounded-lg p-2" onClick={handleClearForm}>Clear</button>
+                        <button className="bg-blue-500 rounded-lg p-2" onClick={generateNewClient}>Add client</button>
 
-                      <label>CVV</label>
-                      <input
-                        type="text"
-                        placeholder="XXXX"
-                        value={newClientForm.credit_card_cvv}
-                        required
-                        onChange={(e) => {
-                          setnewClientForm({
-                            ...newClientForm,
-                            credit_card_cvv: e.target.value,
-                          });
-                        }}
-                        disabled={newClientForm.payment_type === "Zelle"}
-                        />
-
-                      <label>Client Balance</label>
-                      <input
-                        type="text"
-                        placeholder="$"
-                        value={newClientForm.client_balance}
-                        required
-                        onChange={(e) => {
-                          setnewClientForm({
-                            ...newClientForm,
-                            client_balance: e.target.value,
-                          });
-                        }}
-                        />
-                      
                       </div>
                     </div>
-                    <button onClick={generateNewClient}>Submit</button>
                 </form>
               </div>
             </div>
