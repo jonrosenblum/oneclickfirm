6b6c03796d08161b3d442f09611cf576cc92d787
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-15 09:15:33 -0500

    MERGE WITH DEV

--- client/src/Components/Pieces/ActionsDropdown.jsx
+++ client/src/Components/Pieces/ActionsDropdown.jsx
@@ -1,42 +1,40 @@
 /* eslint-disable react/prop-types */
-export default function ActionsDropdown({onClick}) {
+export default function ActionsDropdown({ onClick }) {
     return (
-  
-      <ul
-        className="py-2 text-sm text-gray-700 "
-      >
-        <li>
-          <a
-                onClick={() => onClick('dashboard')}
-            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
-          >
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a
-                onClick={() => onClick('settings')}
-            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
-          >
-            Settings
-          </a>
-        </li>
-        <li>
-          <a
-                onClick={() => onClick('earnings')}
-            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
-          >
-            Earnings
-          </a>
-        </li>
+      <ul className="py-2 text-sm text-gray-700 whitespace-nowrap">
         <li>
           <a
             onClick={() => onClick('download')}
             className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
           >
-            Download Documents
+            Download documents
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => onClick('settings')}
+            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
+          >
+            Client notes
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => onClick('dashboard')}
+            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
+          >
+            Edit client
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => onClick('earnings')}
+            className="block px-4 py-2 hover:bg-gray-200 hover:text-gray-600 cursor-pointer"
+          >
+            Send email
           </a>
         </li>
       </ul>
-   )
+    );
   }
+  
