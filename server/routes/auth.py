044f3852d92fdfde1f5c91adbd6f543d8597c094
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-11-16 18:49:12 -0500

    DB ERRORS

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -16,7 +16,6 @@ from flask_bcrypt import Bcrypt
 auth_bp = Blueprint('auth', __name__)
 
 
-# Establish a connection to the database
 try:
     conn = psycopg2.connect(config('DATABASE_URL'))  # Replace with your actual database URI
     cursor = conn.cursor()
@@ -35,76 +34,100 @@ try:
     ''')
     conn.commit()
 
-except Exception as e:
+except psycopg2.Error as e:
     print("Error connecting to the database:", str(e))
     conn = None
     cursor = None
     keyed_cursor = None
+
+finally:
+    # Close cursors and connection in all scenarios
+    if cursor:
+        cursor.close()
+    if keyed_cursor:
+        keyed_cursor.close()
+    if conn:
+        conn.close()
+        
 
 def getBcrypter():
     return Bcrypt(current_app)
 
 @auth_bp.route("/signup", methods=["POST"])
 def signup():
-    # Get user information from the request
-    first_name = request.json.get("first_name", None)
-    last_name = request.json.get("last_name", None)
-    username = request.json.get("username", None)
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
+    try:
+        # Get user information from the request
+        first_name = request.json.get("first_name", None)
+        last_name = request.json.get("last_name", None)
+        username = request.json.get("username", None)
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
 
+        # Check if required fields are provided
+        if not first_name or not last_name or not username or not email or not password:
+            return jsonify({"msg": "All fields (First Name, Last Name, Username, Email, Password) are required"}), 400
 
-    # Check if required fields are provided
-    if not first_name or not last_name or not username or not email or not password:
-        return jsonify({"msg": "All fields (First Name, Last Name, Username, Email, Password) are required"}), 400
+        # Check for uniqueness of both email and username
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM users WHERE email = %s OR username = %s", (email, username))
+            existing_user = cursor.fetchone()
 
-    # Check for uniqueness of both email and username
-    
-    cursor.execute("SELECT * FROM users WHERE email = %s OR username = %s", (email, username))
-    existing_user = cursor.fetchone()
+            if existing_user:
+                return jsonify({"msg": "Email or Username already exists"}), 400
 
-    if existing_user:
-        return jsonify({"msg": "Email or Username already exists"}), 400
+            # Hash the password before storing it in the database
+            hashed_password = getBcrypter().generate_password_hash(password).decode('utf-8')
 
-    # Hash the password before storing it in the database
-    # hashed_password = password
-    hashed_password = getBcrypter().generate_password_hash(password).decode('utf-8')
+            cursor.execute("INSERT INTO users (first_name, last_name, username, email, password) VALUES (%s, %s, %s, %s, %s)", (first_name, last_name, username, email, hashed_password))
+            conn.commit()
 
-    cursor.execute("INSERT INTO users (first_name, last_name, username, email, password) VALUES (%s, %s, %s, %s, %s)", (first_name, last_name, username, email, hashed_password))
-    conn.commit()
+        # Generate access and refresh tokens
+        access_token = create_access_token(identity=email)
+        refresh_token = create_refresh_token(identity=email)
 
-    # Generate access and refresh tokens
-    access_token = create_access_token(identity=email)
-    refresh_token = create_refresh_token(identity=email)
+        return jsonify(access_token=access_token, refresh_token=refresh_token), 201
 
-    return jsonify(access_token=access_token, refresh_token=refresh_token), 201
+    except psycopg2.OperationalError as e:
+        current_app.logger.error("Database connection error: %s", str(e))
+        return jsonify({"error": "A database error occurred. Please try again later."}), 500
 
+    except Exception as e:
+        current_app.logger.error("An unexpected error occurred: %s", str(e))
+        return jsonify({"error": "An unexpected error occurred. Please try again later."}), 500
 
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
+    try:
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
 
-    keyed_cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
-    # cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
-    user_data = keyed_cursor.fetchone()
-    print("User data:", user_data)
-    # return jsonify(user_data=user_data), 200
-    
-    if user_data == None :
-        return jsonify({"msg": "email not found",'user_data':user_data}), 404
-    
-    # hash = user_data[5]
-    hash = user_data['password']
-    password_valid = getBcrypter().check_password_hash(hash, password)
+        # Assuming you have a connection pool setup
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as keyed_cursor:
+            keyed_cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+            user_data = keyed_cursor.fetchone()
 
-    if user_data and password_valid:
-        access_token = create_access_token(identity=email)
-        refresh_token = create_refresh_token(identity=email)
-        return jsonify(access_token=access_token, refresh_token=refresh_token), 200
-    else:
-        return jsonify({"msg": "Bad password",'user_data':user_data,'hash':hash}), 401
+        if user_data is None:
+            return jsonify({"msg": "email not found"}), 404
+
+        stored_password_hash = user_data['password']
+        password_valid = getBcrypter().check_password_hash(stored_password_hash, password)
+
+        if password_valid:
+            access_token = create_access_token(identity=email)
+            refresh_token = create_refresh_token(identity=email)
+            return jsonify(access_token=access_token, refresh_token=refresh_token), 200
+        else:
+            return jsonify({"msg": "Bad password"}), 401
+
+    except psycopg2.OperationalError as e:
+        current_app.logger.error("Database connection error: %s", str(e))
+        return jsonify({"error": "A database error occurred. Please try again later."}), 500
+
+    except Exception as e:
+        current_app.logger.error("An unexpected error occurred: %s", str(e))
+        return jsonify({"error": "An unexpected error occurred. Please try again later."}), 500
+
 
 @auth_bp.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
@@ -127,56 +150,3 @@ def refresh():
 
 
 
-
-
-
-
-
-
-
-
-# from flask import Blueprint, request, jsonify
-
-# from flask_jwt_extended import create_access_token
-# from flask_jwt_extended import get_jwt_identity
-# from flask_jwt_extended import jwt_required
-
-# auth_bp = Blueprint('auth', __name__)
-
-# @auth_bp.route('/authentication', methods=['GET'])
-# def authenticationTest():
-#     return "Authentication Backend!"
-
-# @auth_bp.route('/login', methods=['POST'])
-# def login():
-#     email = request.form['username']
-#     password = request.form['password']
-#     if email == 'test@gmail.com' and password == 'password':
-        
-#         return jsonify({'status':'success','message': 'Login successful navigating to /dashboard'})
-#     else:
-#         return jsonify({'status': 'fail','message': 'Invalid username or password'})
-    
-    # Create a route to authenticate your users and return JWTs. The
-# create_access_token() function is used to actually generate the JWT.
-
-
-# @auth_bp.route("/login", methods=["POST"])
-# def login():
-#     username = request.json.get("username", None)
-#     password = request.json.get("password", None)
-# #     if username != "test" or password != "test":
-# #         return jsonify({"msg": "Bad username or password"}), 401
-        
-# #     access_token = create_access_token(identity=username)
-# #     return jsonify(access_token=access_token)
-
-
-# # Protect a route with jwt_required, which will kick out requests
-# # without a valid JWT present.
-# @auth_bp.route("/protected", methods=["GET"])
-# @jwt_required()
-# def protected():
-#     # Access the identity of the current user with get_jwt_identity
-#     current_user = get_jwt_identity()
-#     return jsonify(logged_in_as=current_user), 200
